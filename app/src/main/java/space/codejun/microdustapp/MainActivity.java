3f18f3dfee957e4a5679fd29aa0a6d813ab5cc62
Junseo Bang <junseo1020@gmail.com>
2018-05-09 22:22:06 +0900

    Add Firebase FireStore and Test!

--- app/src/main/java/space/codejun/microdustapp/MainActivity.java
+++ app/src/main/java/space/codejun/microdustapp/MainActivity.java
@@ -2,30 +2,61 @@ package space.codejun.microdustapp;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.CompoundButtonCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 
 import com.gelitenight.waveview.library.WaveView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
     private WaveHelper mWaveHelper;
 
+    private static String TAG = "MainActivity";
+
     private int mBorderColor = Color.parseColor("#44FFFFFF");
     private int mBorderWidth = 5;
     private int waterLevel = 15;
+
+    private WaveView waveView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        final WaveView waveView = findViewById(R.id.wave);
+        db.collection("pm2_5")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
 
+        waveView = findViewById(R.id.wave);
         mWaveHelper = new WaveHelper(waveView);
+
+        loadDustLevel();
+    }
+
+    public void loadDustLevel() {
         waveView.setShapeType(WaveView.ShapeType.CIRCLE);
         waveView.setBorder(mBorderWidth, mBorderColor);
         waveView.setWaveColor(Color.parseColor("#3F51B5"), Color.parseColor("#303F9F"));
@@ -33,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
         if (waterLevel <= 15) {
             waveView.setWaveColor(Color.parseColor("#69F0AE"), Color.parseColor("#00E676"));
         }
-
     }
 
     @Override

4c65baf4bb6d4f2047464d38bfb1bf4a63a59391
Soboro <junseo1020@gmail.com>
2018-05-11 22:42:58 +0900

    농도를 표시하기 위한 뷰 라이브러리 변경

--- app/src/main/java/space/codejun/microdustapp/MainActivity.java
+++ app/src/main/java/space/codejun/microdustapp/MainActivity.java
@@ -12,7 +12,6 @@ import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-import com.gelitenight.waveview.library.WaveView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
@@ -24,14 +23,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WaveHelper mWaveHelper;
-
     private static String TAG = "MainActivity";
-
-    private int mBorderColor = Color.parseColor("#44FFFFFF");
-    private int mBorderWidth = 5;
-
-    private WaveView waveView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +31,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        waveView = findViewById(R.id.wave);
-        mWaveHelper = new WaveHelper(waveView);
         final TextView textView = findViewById(R.id.dust);
 
         final DocumentReference docRef = db.collection("pm2_5").document("FpvuP0Plq2vmk8FSWMEh");
@@ -58,40 +48,10 @@ public class MainActivity extends AppCompatActivity {
                     // cutNumber(String.valueOf(snapshot.getData()));
                     String dust = snapshot.getString("pm2_5");
                     textView.setText(dust);
-                    loadDustLevel(Integer.valueOf(dust));
                 } else {
                     Log.d(TAG, "Current data: null");
                 }
             }
         });
-    }
-
-    public void loadDustLevel(Integer level) {
-        waveView.setShapeType(WaveView.ShapeType.CIRCLE);
-        waveView.setBorder(mBorderWidth, mBorderColor);
-        waveView.setWaveColor(Color.parseColor("#3F51B5"), Color.parseColor("#303F9F"));
-        Log.d("RATIO", String.valueOf(waveView.getWaterLevelRatio()));
-
-        if (level <= 15) {
-            waveView.setWaveColor(Color.parseColor("#69F0AE"), Color.parseColor("#00E676"));
-            waveView.setWaterLevelRatio(0.2f);
-        } else if (level > 16) {
-            waveView.setWaterLevelRatio(0.5f);
-        }
-        if (level > 30) {
-            waveView.setWaterLevelRatio(0.8f);
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mWaveHelper.cancel();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mWaveHelper.start();
     }
 }

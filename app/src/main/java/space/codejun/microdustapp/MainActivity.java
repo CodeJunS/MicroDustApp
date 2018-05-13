4a3c9694d945a8745446ce3ce28a07804cac16cd
Junseo Bang <junseo1020@gmail.com>
2018-05-13 21:28:30 +0900

    Arrange import

--- app/src/main/java/space/codejun/microdustapp/MainActivity.java
+++ app/src/main/java/space/codejun/microdustapp/MainActivity.java
@@ -2,25 +2,12 @@ package space.codejun.microdustapp;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.widget.CompoundButtonCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
 import com.github.lzyzsd.circleprogress.ArcProgress;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
